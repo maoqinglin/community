ae246d677e25f476c17129e86118717f4fce0294
xiaolin <linmq@snailgame.net>
2019-07-11 17:59:29 +0800

    init repository

---
+++ src/main/java/com/ireadygo/community/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.ireadygo.community.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello"; // 需要引入 thymeleaf，否则报javax.servlet.ServletException: Circular view path [hello]:
+    }
+}
