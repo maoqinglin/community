6d531a2fb51c167ffd8c28c1777a0c4b40f277fc
xiaolin <linmq@snailgame.net>
2019-07-11 18:25:01 +0800

    add README.md

--- src/main/java/com/ireadygo/community/controller/HelloController.java
+++ src/main/java/com/ireadygo/community/controller/HelloController.java
@@ -1,6 +1,5 @@
 package com.ireadygo.community.controller;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
